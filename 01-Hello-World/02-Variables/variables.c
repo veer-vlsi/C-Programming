f652129d7baa23813dca9b39ca35d7d2fdedc745
Veer <veereshnm99@gmail.com>
2026-07-22 04:06:15 +0530

    Create variables.c

---
+++ 01-Hello-World/02-Variables/variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int age = 21;
+    float cgpa = 8.5;
+    char grade = 'A';
+
+    printf("Age: %d\n", age);
+    printf("CGPA: %.1f\n", cgpa);
+    printf("Grade: %c\n", grade);
+
+    return 0;
+}
